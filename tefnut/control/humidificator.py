f5a00ed2bc3dfe1641c1cd6662c40eacc68bc1e6
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2022-11-14 21:35:06 -0500

    fix renaming

--- tefnut/control/humidificator.py
+++ tefnut/control/humidificator.py
@@ -4,10 +4,10 @@ from tefnut.utils.setting import settings
 class Humidificator:
     def __init__(self):
         if settings.get("GENERAL.humidificator") == "pi":
-            from tefnut.control.humidificator_pi import HumidificatorImplement
+            from tefnut.control.humi_pi import HumidificatorImplement
             self.humi = HumidificatorImplement()
         else:
-            from tefnut.control.humidificator_stub import HumidificatorImplement
+            from tefnut.control.humi_stub import HumidificatorImplement
             self.humi = HumidificatorImplement()
 
     def turn_on(self):
