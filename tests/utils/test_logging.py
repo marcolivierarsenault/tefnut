f0f54998f8883ce2139bc3e8234dc8fcf7ec276b
Marc-Olivier Arsenault <marcolivier.arsenault@shopify.com>
2022-11-15 14:03:33 -0500

    add failing test

--- tests/utils/test_logging.py
+++ tests/utils/test_logging.py
@@ -4,4 +4,4 @@ from tefnut.utils.logging import configure_logger
 
 def test_disable_loki():
     configure_logger()
-    assert len(logging.getLogger("main").handlers) == 1
+    assert len(logging.getLogger("main").handlers) == 10
