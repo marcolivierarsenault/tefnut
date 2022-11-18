56d807a4e2624b918550a691a0f3ac6625cf3ea7
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2022-11-18 06:51:30 -0500

    start building webui

--- tefnut.py
+++ tefnut.py
@@ -4,10 +4,23 @@ Tenut
 import logging
 import threading
 import git
+import tefnut.webui.webservice as webservice
 from tefnut.control import control
 from tefnut.utils.logging import configure_logger
 
 logger = logging.getLogger("main")
+
+
+def start_control():
+    logger.debug("Starting control loop")
+    x = threading.Thread(target=control.control_loop, args=(1,))
+    x.start()
+
+
+def start_webui():
+    logger.debug("Starting control loop")
+    webservice.app.run(host='0.0.0.0', debug=True)
+
 
 if __name__ == "__main__":
     logger.info("++++++++++Tefnut application starting++++++++++")
@@ -16,6 +29,6 @@ if __name__ == "__main__":
     repo = git.Repo(search_parent_directories=True)
     sha = repo.head.object.hexsha
     logger.info("Starting code on git sha: %s", sha)
-    logger.debug("Starting control loop")
-    x = threading.Thread(target=control.control_loop, args=(1,))
-    x.start()
+    
+    # start_control()
+    start_webui()
