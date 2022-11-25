14e7faa4a552cd033f0f31759527ae0315d61b2e
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2022-11-24 21:58:50 -0500

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+echo Deploying code $1
+
+
+echo stopping service
+systemctl stop tefnut.service
+
+cd /home/marco/tefnut
+echo updating code
+git checkout main
+git pull --all
+echo checkout specific code
+git checkout $1
+
+
+echo Starting service
+systemctl start tefnut.service
+
+echo ===============================================
+journalctl -n 30 -u tefnut.service
+echo ===============================================
+sudo systemctl status tefnut.service
+echo ===============================================
+
+echo done
