56e2206f3f2222129257f5858d67ad7fe1cd1d8d
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2022-12-22 11:20:51 -0500

    add influx testing

--- tests/utils/test_influx_client.py
+++ tests/utils/test_influx_client.py
@@ -1,10 +1,18 @@
 import pytest
+from tefnut.utils.setting import settings
 from tefnut.utils.influx_client import InfluxClient
+from unittest.mock import patch
 from influxdb_client import Point
 
 
 @pytest.fixture
 def influx_db():
+    return InfluxClient()
+
+
+@pytest.fixture
+def working_influx_db():
+    settings.set("INFLUX.enable", True, persist=False)
     return InfluxClient()
 
 
@@ -15,3 +23,32 @@ def point():
 
 def test_write_does_not_write_with_stub(influx_db, point):
     assert influx_db.write(point) == 1
+
+
+def test_influx_connection_test_write(working_influx_db, point):
+    with patch(
+            "influxdb_client.client.write_api.WriteApi.write"
+            ) as write_api:
+        assert working_influx_db.write(point) == 0
+        write_api.assert_called_once()
+
+
+def test_influx_connection_raise_exeption_without_mock(working_influx_db, point, caplog):
+    with patch(
+            "influxdb_client.client.write_api.WriteApi.write"
+            ) as write_api:
+        write_api.side_effect = Exception("test")
+        assert working_influx_db.write(point) == -1
+        write_api.assert_called_once()
+        assert 'Faillure to configure Influx DB client' in caplog.text
+
+
+def test_influx_failing_to_create_client(caplog):
+    settings.set("INFLUX.enable", True, persist=False)
+    with patch(
+            "influxdb_client.client.influxdb_client.InfluxDBClient.__init__"
+            ) as influx_client_mock:
+        influx_client_mock.side_effect = Exception("test")
+        InfluxClient()
+        influx_client_mock.assert_called_once()
+        assert 'Faillure to configure Influx DB client' in caplog.text
