7bec435b1facad02844e4ae1c47e6b2e9586cb2f
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2023-08-12 19:39:40 -0400

    test new version

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,28 @@ echo Deploying code $1
 echo stopping service
 systemctl stop tefnut.service
 
-cd /home/marco/tefnut
-echo updating code
-git checkout main
-git pull --all
-echo checkout specific code
-git checkout $1
 
 /home/marco/.local/bin/poetry install
 
 echo Starting service
 systemctl start tefnut.service
 
+if (! systemctl -q is-active tefnut.service)
+    then
+    echo "Application failed to start"
+    exit -1
+fi
+
 echo ===============================================
 journalctl -n 30 -u tefnut.service
 echo ===============================================
-sudo systemctl status tefnut.service
-echo ===============================================
+
+echo sleeping 10 seconds
+sleep 10
+if (! systemctl -q is-active tefnut.service)
+    then
+    echo "Application failed after 10 seconds"
+    exit -1
+fi
 
 echo done
