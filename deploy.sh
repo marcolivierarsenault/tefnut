bda1235007556b712a731854473a1ab46ab1b413
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2023-08-13 19:59:10 -0400

    upgrade deploy logic (#81)
    * test deploy
    
    * add true
    
    * add true
    
    * test script
    
    * test new version
    
    * update python version
    
    * add pyenv
    
    * add sudo
    
    * test version
    
    * test version
    
    * test version
    
    * ssh key
    
    * move delay
    
    * move delay
    
    * adding timestamps
    
    * remove debug
    
    * change readme

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,50 @@
 #! /bin/bash
 
-echo Deploying code $1
+echo Deploying code $1 | ts
 
 
-echo stopping service
-systemctl stop tefnut.service
+echo stopping service | ts
+sudo systemctl stop tefnut.service
 
-cd /home/marco/tefnut
-echo updating code
-git checkout main
-git pull --all
-echo checkout specific code
-git checkout $1
 
 /home/marco/.local/bin/poetry install
 
-echo Starting service
-systemctl start tefnut.service
+echo Starting service| ts
+sudo systemctl start tefnut.service
+
+echo checking status| ts
+if (! sudo systemctl -q is-active tefnut.service)
+    then
+    echo "Application failed to start"| ts
+    exit -1
+    else
+    echo "Application started"| ts
+fi
+echo sleeping 60 seconds| ts
+sleep 60
 
 echo ===============================================
 journalctl -n 30 -u tefnut.service
 echo ===============================================
-sudo systemctl status tefnut.service
-echo ===============================================
 
-echo done
+
+
+echo checking version| ts
+if (journalctl -n 30 -u tefnut.service | grep -q $1 )
+    then
+    echo "Good version started"| ts
+    else
+    echo "Not on good version"| ts
+    exit -1
+fi
+
+echo checking status| ts
+if (! sudo systemctl -q is-active tefnut.service)
+    then
+    echo "Application failed after 10 seconds"| ts
+    exit -1
+    else
+    echo "Application started after 10 seconds"| ts
+fi
+
+echo done| ts
